dceb76d98727ba143a82795d4e31fb922236a791
keeemhs <alenen0227@naver.com>
2023-10-27 14:35:38 +0900

    231027 commit

---
+++ CodingTestPrac/src/Activity2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Activity2 {
+
+    public static int[] findIndices(int[] nums, int target) {
+        Map<Integer, Integer> numToIndex = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numToIndex.containsKey(complement)) {
+                return new int[]{numToIndex.get(complement), i};
+            }
+            numToIndex.put(nums[i], i);
+        }
+        
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums2 = {2, 4, 3, 6, 8, 5, 5, 2};
+        int target2 = 6;
+        int[] result2 = findIndices(nums2, target2);
+        
+        if (result2 != null) {
+            System.out.println("[" + result2[0] + ", " + result2[1] + "]");
+        } else {
+            System.out.println("해답이 존재하지 않습니다.");
+        }
+    }
+}
